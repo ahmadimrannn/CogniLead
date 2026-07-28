6d2b42ba8d136e796f590d54901518c1770db7b2
Ahmad Imran <ahmadimran67208@gmail.com>
2026-07-28 19:39:36 +0500

    Add api endpoint for resume handling

--- src/cognilead/api/main.py
+++ src/cognilead/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, status
 from graph.graph_executor import execute_graph
+from graph.resume_graph import resume_graph
 from pydantic import BaseModel, Field
+import uuid
 
 app = FastAPI(title="Lead Qualification API")
 
@@ -12,6 +14,10 @@ class LeadProcessRequest(BaseModel):
     examples=["We're a 40-person Series A fintech... can we get a demo?"]
   )
 
+class ResumeRequest(BaseModel):
+  thread_id: str
+  action: str
+
 
 @app.post("/leads")
 def process_leads(request: LeadProcessRequest):
@@ -22,9 +28,9 @@ def process_leads(request: LeadProcessRequest):
       detail="Can't process the lead without the lead details."
     )
 
-
+  thread_id = str(uuid.uuid4())
   try:
-    response = execute_graph(text)
+    response = execute_graph(text, thread_id)
     return response
   except Exception as e:
     print(str(e))
@@ -33,4 +39,32 @@ def process_leads(request: LeadProcessRequest):
       detail=f"Failed to process the lead due to some internal error."
     )
 
+
+@app.post("/leads/resume")
+def resume_workflow(request: ResumeRequest):
+  
+  if request.action not in {"approve", "reject"}:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail="Invalid Action"
+    )
+  
+
+  try:
+    result = resume_graph(
+      action=request.action,
+      thread_id=request.thread_id,
+      edited_query=request.edited_query
+    )
+
+    return result
+
+  except Exception as e:
+    print(str(e))
+    raise HTTPException(
+      status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+      detail=f"Can't fetch results."
+    )
+  
+  
 
